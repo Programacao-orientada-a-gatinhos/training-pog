b4f7586f8e62e6167cf4b19b61db2897573e6127
M4tiasToreta <toretamatias218@gmail.com>
2025-07-07 01:25:43 -0300

    trying to optmize this shit

--- uri1926.cpp
+++ uri1926.cpp
@@ -6,23 +6,29 @@ using namespace std;
 
 const int MAX = 50001;
 vector<int> adj[MAX];
-int a, b, sequence[MAX][2], answer, n, depth, card, visited[MAX], curr_gen=0;
+int a, b, sequence[MAX], answer, n, depth, card;
 
 int bfs (int s) {
     queue<int> q;
-    q.push(s);
-    sequence[s][1]=0;
+    int visited[MAX];
+    fill(visited, visited + MAX, 0);
+    q.push(s); // primeiro nessa merda de fila
+    visited[s]=1;
 
     while (!q.empty()) {
-        int curr = q.front();
-        q.pop();
-        for (int neighbor : adj[curr]) {
-            if (visited[neighbor]==curr_gen) {
-                visited[neighbor]=curr_gen;
-                sequence[neighbor][1] = sequence[curr][1]+1;
-                if (sequence[neighbor][0] == card) {
-                    return sequence[neighbor][1];
+        int curr = q.front(); // pega o primeiro
+        q.pop(); // tira o primeiro
+        for (int neighbor : adj[curr]) { // itera em todos os vizinhos da carta em questão
+            printf("gen> %d ", visited[curr]);
+            printf("neighbor> %d ", neighbor);
+            printf("visited? %d\n", visited[neighbor]);
+            if (!visited[neighbor]) {
+                if (sequence[neighbor] == card) {
+                    return visited[curr];
                 } else {
+                    visited[neighbor] = visited[curr] + 1;
+                    printf("gen atributted> %d\n", visited[neighbor]);
+                    printf("rodou\n"); 
                     q.push(neighbor);
                 }
             }
@@ -34,7 +40,7 @@ int bfs (int s) {
 int main(){
     scanf("%d", &n);
     for (int i=1; i<=n; i++) {
-        scanf("%d", &sequence[i][0]);
+        scanf("%d", &sequence[i]);
     }
     while (scanf(" %d %d", &a, &b) != EOF){
         adj[a].push_back(b);
@@ -42,12 +48,11 @@ int main(){
     }
     unordered_set<int> seen_cards;
     for (int j=1; j<=n; j++) {
-        if (seen_cards.count(sequence[j][0]) == 0) {
-            seen_cards.insert(sequence[j][0]);
-            sequence[j][1] = 0;
-            card = sequence[j][0];
-            curr_gen++;
+        if (seen_cards.count(sequence[j]) == 0) {
+            seen_cards.insert(sequence[j]);
+            card = sequence[j];
             int result = bfs(j);
+            // printf("%d", result);
             answer += result;
         }
     }
